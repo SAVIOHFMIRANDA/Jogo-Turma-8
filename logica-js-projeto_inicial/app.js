ba97b3279d47eefa651c50d3adbed40f3f8e5a8f
SAVIOHFMIRANDA <saviohfm@gmail.com>
2025-02-04 22:05:23 -0300

    Add files via upload

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,6 @@
+alert("Boas Vindas ao jogo do número secreto");
+let numeroSecreto = 7;
+let chute = prompt("escolha um numero entre 1 a 10");
+if (chute == numeroSecreto) {
+    console.log("Isso ai! Você descobriu o numero secreto (7)");
+}
